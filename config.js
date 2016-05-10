debc2994f3fa6e92f16baeaf304503b87a7efe8b
John Ritsema <john_ritsema@yahoo.com>
2016-05-10 16:12:15 -0400

    replaced config settings with environment variables

--- config.js
+++ config.js
@@ -1,13 +1,11 @@
-  // Don't commit this file to your public repos. This config is for first-run
-  //
- exports.creds = {
-  realm: '<your realm>', // Replace with your tenant name xxxx where xxxx is xxxx.onmicrosoft.com / xxxx.sharepoint.com
- 	returnURL: 'http://localhost:3000/auth/openid/return',
- 	identityMetadata: 'https://login.microsoftonline.com/common/.well-known/openid-configuration', // For using Microsoft you should never need to change this.
- 	clientID: 'c9655d1d-f356-46a7-afe1-431c0d6eeb37',
- 	clientSecret: '4+47R51b9PJA15G6G5C1ZP7oayV0IMKNZ5lrYN6pVIc=', // if you are doing code or id_token code
- 	skipUserProfile: true, // for AzureAD should be set to true.
- 	responseType: 'id_token code', // for login only flows use id_token. For accessing resources use `id_token code`
- 	responseMode: 'query', // For login only flows we should have token passed back to us in a POST
- 	//scope: ['email', 'profile'] // additional scopes you may wish to pass
- };
+exports.creds = {
+  realm: 'turner.onmicrosoft.com', // Replace with your tenant name xxxx where xxxx is xxxx.onmicrosoft.com / xxxx.sharepoint.com
+  identityMetadata: 'https://login.microsoftonline.com/common/.well-known/openid-configuration', // For using Microsoft you should never need to change this.
+  skipUserProfile: true, // for AzureAD should be set to true.
+  responseType: 'id_token code', // for login only flows use id_token. For accessing resources use `id_token code`
+  responseMode: 'query', // For login only flows we should have token passed back to us in a POST
+  //scope: ['email', 'profile'] // additional scopes you may wish to pass
+  returnURL: process.env.RETURN_URL, //needs to be set in azure ad portal; ex: http://localhost:9000/auth/openid/return
+  clientID: process.env.CLIENT_ID, //get from azure ad portal
+  clientSecret: process.env.CLIENT_SECRET, //if you are doing code or id_token code; get from azure ad portal
+};
