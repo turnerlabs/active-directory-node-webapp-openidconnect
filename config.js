c303ce2cc0e2e51d9274149f63716ffabb2b2252
John Ritsema <john_ritsema@yahoo.com>
2016-05-10 14:28:55 -0400

    added missing realm to config.js

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
   // Don't commit this file to your public repos. This config is for first-run
   //
  exports.creds = {
+  realm: '<your realm>', // Replace with your tenant name xxxx where xxxx is xxxx.onmicrosoft.com / xxxx.sharepoint.com
  	returnURL: 'http://localhost:3000/auth/openid/return',
  	identityMetadata: 'https://login.microsoftonline.com/common/.well-known/openid-configuration', // For using Microsoft you should never need to change this.
  	clientID: 'c9655d1d-f356-46a7-afe1-431c0d6eeb37',
